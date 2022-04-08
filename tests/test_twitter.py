2181eceea87359961263ecc9120e98d927ef9ae4
Andrey Zavodov <a.p.zavodov@gmail.com>
2022-04-08 11:22:00 +0300

    Add api tests for twitter

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -14,6 +14,28 @@ class TestTwitter(unittest.TestCase):
         self.api = Twitter(app_key=app_key, app_secret=app_secret,
                            oauth_token=oauth_token, oauth_token_secret=oauth_token_secret)
 
+    def test_get_mentions_timeline(self):
+        self.api.get_mentions_timeline()
+
+    def test_get_user_timeline(self):
+        self.api.get_user_timeline()
+        self.api.get_user_timeline(screen_name='twitter')
+
+    def test_retweeted_of_me(self):
+        self.api.retweeted_of_me()
+
+    def test_get_retweets(self):
+        self.api.get_retweets(id="99530515043983360")
+
+    def test_search(self):
+        self.api.search(q='twitter')
+
     def test_get_followers_ids(self):
-        ids = self.api.get_followers_ids(screen_name="github")
-        self.assertGreater(len(ids), 0)
+        self.api.get_followers_ids(screen_name="github")
+
+    def test_get_privacy_policy(self):
+        self.api.retweeted_of_me()
+
+    def test_get_application_rate_limit_status(self):
+        self.api.get_application_rate_limit_status()
+
