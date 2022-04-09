a971b29672f594f463fe2db3b3aa39856bf47bd8
Andrey Zavodov <a.p.zavodov@gmail.com>
2022-04-09 14:24:00 +0300

    Add api tests for twitter v2

--- tests/test_twitter_v2.py
+++ tests/test_twitter_v2.py
@@ -10,7 +10,12 @@ class TestTwitter2(unittest.TestCase):
         access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
         self.api = Twitter2(access_token=access_token)
 
-    def testGetTweets(self):
-        tweets = self.api.get_tweets(ids=['1261326399320715264', '1278347468690915330'])
-        self.assertGreater(len(tweets), 0)
+    def test_get_tweet(self):
+        self.api.get_tweet(id='1261326399320715264')
+
+    def test_get_tweets(self):
+        self.api.get_tweets(ids=['1261326399320715264', '1278347468690915330'])
+
+    def test_get_compliance_jobs(self):
+        self.api.get_compliance_jobs(type='tweets')
 
