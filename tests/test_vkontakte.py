4febbeb27ad90c1aed0dd22588fa2380c7fe9d4e
Andrey Zavodov <a.p.zavodov@gmail.com>
2022-04-10 16:43:00 +0300

    Add api tests for vk

--- tests/test_vkontakte.py
+++ tests/test_vkontakte.py
@@ -10,7 +10,13 @@ class TestVkontakte(unittest.TestCase):
         access_token = os.environ.get("VKONTAKTE_ACCESS_TOKEN")
         self.api = Vkontakte(access_token=access_token)
 
-    def testSearchUsers(self):
-        users = self.api.search_users(q="Иван Иванов")['response']['items']
-        self.assertGreater(len(users), 0)
+    def test_get_account_counters(self):
+        self.api.account_get_counters(filter="friends_recommendations")
+        self.api.account_get_push_settings()
+
+    def test_search_users(self):
+        self.api.users_search(q="Иван Иванов", count=7)
+
+
+
 
