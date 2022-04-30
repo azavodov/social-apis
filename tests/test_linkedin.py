8be394e5ed487bf3b43162022f49c2d2ae7ca944
Andrey Zavodov <a.p.zavodov@gmail.com>
2022-04-30 11:23:00 +0300

    Refactor linkedin tests - add requires header

--- tests/test_linkedin.py
+++ tests/test_linkedin.py
@@ -7,8 +7,12 @@ from config import ld_access_token
 class TestLinkedin(unittest.TestCase):
 
     def setUp(self):
-        self.api = Linkedin(access_token=ld_access_token)
+        self.api = Linkedin(access_token=ld_access_token,
+                            client_args={'headers': {'X-RestLi-Protocol-Version': '2.0.0'}})
 
-    def testPlaces(self):
+    def test_me_endpoint(self):
+        self.assertIn('id', self.api.me())
+
+    def test_regions(self):
         places = self.api.regions(count=10)['elements']
         self.assertEqual(len(places), 10)
