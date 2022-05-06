8618fa5ff0d5083f78ce089b89b5e85db230511a
Andrey Zavodov <a.p.zavodov@gmail.com>
2022-05-06 10:30:00 +0300

    Add tests for vk api

--- tests/test_vkontakte.py
+++ tests/test_vkontakte.py
@@ -11,11 +11,33 @@ class TestVkontakte(unittest.TestCase):
 
     def test_get_account_counters(self):
         self.api.account_get_counters(filter="friends_recommendations")
+
+    def test_account_get_push_settings(self):
         self.api.account_get_push_settings()
 
-    def test_search_users(self):
+    def test_users_search(self):
         self.api.users_search(q="Иван Иванов", count=7)
 
+    def test_database_get_countries(self):
+        self.api.database_get_countries(count=7)
 
+    def test_database_get_regions(self):
+        self.api.database_get_regions(country_id='1', count=5)
 
+    def test_database_get_cities(self):
+        self.api.database_get_cities(country_id='1')
 
+    def test_database_get_universities(self):
+        self.api.database_get_universities(city_id='1', count=7)
+
+    def test_database_get_faculties(self):
+        self.api.database_get_faculties(university_id='1', count=3)
+
+    def test_database_get_chairs(self):
+        self.api.database_get_chairs(faculty_id='1', count=5)
+
+    def test_database_get_sc(self):
+        self.api.database_get_schools(city_id='1', count=5)
+
+    def test_database_get_school_classes(self):
+        self.api.database_get_school_classes(school_id='1', count=1)
