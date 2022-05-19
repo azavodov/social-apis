283b57d6bd4a53bf615e50ba35f1f543f3db72f7
Andrey Zavodov <a.p.zavodov@gmail.com>
2022-05-19 17:43:54 +0300

    Update description and set release version

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     license='MIT',
     author='Andrey Zavodov',
     author_email='a.p.zavodov@gmail.com',
-    description='Make requests to different APIs with one library',
+    description='Social APIs is a library providing an easy way to access Social Networks APIs.',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    keywords='social networks API',
+    keywords='social networks API requests access automation',
     zip_safe=False
 )
--- src/social_apis/__init__.py
+++ src/social_apis/__init__.py
@@ -1,2 +1,2 @@
 __author__ = 'Andrey Zavodov <a.p.zavodov@gmail.com>'
-__version__ = '0.5'
+__version__ = '1.0'
